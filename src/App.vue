01af8a056bacc2ef00ae7b21a027bef4a4f5ffe2
ssshooter <472356884@qq.com>
2019-08-17 08:57:18 +0800

    Re:new

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div v-my-directive></div>
 </template>
 
 <script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,8 @@
-var path = require('path')
-var webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js',
-  },
   module: {
     rules: [
       {
@@ -16,9 +10,14 @@ module.exports = {
         loader: 'vue-loader',
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.css$/,
@@ -34,73 +33,10 @@ module.exports = {
     ],
   },
   plugins: [
-    // 请确保引入这个插件！
     new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'vue plugin demo',
+      template: 'index.html',
+    }),
   ],
-  resolve: {
-    // alias: {
-    //   vue$: 'vue/dist/vue.esm.js'
-    // },
-    extensions: ['*', '.js', '.vue', '.json'],
-  },
-  devServer: {
-    host: '0.0.0.0',
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true,
-  },
-  performance: {
-    hints: false,
-  },
-  devtool: '#eval-source-map',
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"',
-      },
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false,
-        drop_console: true,
-      },
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),
-  ])
-}
-
-if (process.env.NODE_ENV === 'vueplugin') {
-  module.exports.entry = './src/gallery'
-  module.exports.output = {
-    path: path.resolve(__dirname, './'),
-    publicPath: '/',
-    libraryTarget: 'umd',
-    filename: 'index.js',
-  }
-  module.exports.devtool = '#source-map'
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"',
-      },
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false,
-        drop_console: true,
-      },
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),
-  ])
 }
